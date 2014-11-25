c73b1722c46bfce9683b53d1f3ab2d91ae7d5e36
timothyl@uw.edu <Tim@d-10-154-192-36.dhcp4.washington.edu>
2014-11-24 21:38:30 -0800

    fixed indentation

--- js/app.js
+++ js/app.js
@@ -15,13 +15,11 @@ $(document).ready(function() {
 	var mapElem = document.getElementById('map');
 	var mapOptions = {
 		center: {lat: 47.6, lng: -122.3},
-		zoom: 12 // 0 = Earth to 21 = Max Zoom
+		zoom: 12 
 	};
-
 
 	// create the map
 	var map = new google.maps.Map(mapElem, mapOptions);
-
 	var infoWindow = new google.maps.InfoWindow();
 
 	var stations;
@@ -30,7 +28,7 @@ $(document).ready(function() {
 	$.getJSON('http://data.seattle.gov/resource/65fc-btcc.json')
 		.done(function(data) {
 			stations = data;
-
+			// Populates map with markers according to latitude/longitude data
 			data.forEach(function(station) {
                 var marker = new google.maps.Marker({
                     position: {
@@ -41,36 +39,43 @@ $(document).ready(function() {
                 });
                 markers.push(marker);
 
+                // Shows info window when a marker is clicked
                 google.maps.event.addListener(marker, 'click', function() {
                 	map.panTo(marker.getPosition());
                 	var html = '<p>' + station.cameralabel + '</p>';
                 	html += '<img src=' + station.imageurl.url + '>';
                 	infoWindow.setContent(html);
                 	infoWindow.open(map,this);
+
+                	// When clicked, marker bounces for about 2 seconds
+                	marker.setAnimation(google.maps.Animation.BOUNCE);
+                	setTimeout(function() {
+                		marker.setAnimation(null);	
+                	}, 2150);
                 });
 
+                // If user clicks on the map, close the info window
+                google.maps.event.addListener(map, 'click', function() {
+                    infoWindow.close();
+                });
+
+                // Filters the markers based on user input
                 $("#search").bind('search keyup', function() {
                 	var query = this.value.toLowerCase();
-                	console.log(query);
-
-                	// removes marker if search query does not exist 
+                	// removes marker if user search input does not match
                 	if (station.cameralabel.toLowerCase().indexOf(query) < 0) {
                 		marker.setMap(null);
-                	// leaves marker in if search phrase exists
+                	// leaves marker in if user search input matches
                 	} else {
                 		marker.setMap(map);
                 	}
-
-                });
+                }); 
 
             });
-			//success
-		})
+		}) 
+
+		// If there is an error loading data
 		.fail(function(error) {
-			window.alert('An error occurred!');
+			window.alert(error);
 		})
-		.always(function() {
-			//called on either success or failure
-		})
- 
 });
