2298da1a426564f0964b0a0bfb3621ff9205f9dd
timothyl@uw.edu <Tim@d-10-154-192-36.dhcp4.washington.edu>
2014-11-24 19:20:10 -0800

    basic structure

--- js/app.js
+++ js/app.js
@@ -11,3 +11,29 @@
 //you should also write the code to filter the set of markers when the user
 //types a search phrase into the search box
 
+$(document).ready(function() {
+	var mapElem = document.getElementById('map');
+	var mapOptions = {
+		center: {lat: 47.6, lng: -122.3},
+		zoom: 12 // 0 = Earth to 21 = Max Zoom
+	};
+
+// add our map to the page in the "map" div
+// create the map
+	var map = new google.maps.Map(mapElem, mapOptions);
+
+
+	var infoWindow = new google.maps.InfoWindow();
+
+	$.getJSON('http://data.seattle.gov/resource/65fc-btcc.json')
+		.done(function(data) {
+			//success
+		})
+		.fail(function(error) {
+			console.log(error);
+		})
+		.always(function() {
+			//called on either success or failure
+		})
+
+});
